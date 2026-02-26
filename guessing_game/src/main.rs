73ecf13ac59d84bca9beb998d4e2960c7c38e7b6
bluffset7340 <sbm7640@outlook.com>
2026-02-26 21:25:18 +0400

    guessing game is now done

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,6 +1,6 @@
-use std::io; 
+use std::io;
 // to use input output we take it from the standard library
-// prelude aims to strike a balance by importing all the essentials that every 
+// prelude aims to strike a balance by importing all the essentials that every
 // rust program might need without having to explicitly import it
 // use rand::Rng; // unused import so commenting it out
 use std::cmp::Ordering;
@@ -10,34 +10,46 @@ fn main() {
     // the exclamation mark calls a macro in Rust
     let sec_num = rand::random_range(1..=100); // generate number from 1 to 100
 
-    println!("The secret number is {sec_num}");
+    // println!("The secret number is {sec_num}");
 
     println!("Please input a number: ");
-
-    // declaring a variable, a mutable variable, whose value can change
-    let mut guess = String::new(); // returns new instance of a string, apply the new function on the string via the :: syntax
 
     // calling the standard input from the io library
     // could be written as std::io::stdin
     // pressing enter adds a newline to the string
-    io::stdin()
-        .read_line(&mut guess) // method to handle input from the user - take what the sax users says and append it to the string 
-        .expect("Failed to real line"); // this runs when the enum result is err, else it returns the input 
-    // read_line() returns an enum and encode info according to two state ok - and err. 
-    // ok is linked to ouput and err linked to error msg and why it failed
-    // curly braces - placeholder for dynamic data
+    loop {
+        // declaring a variable, a mutable variable, whose value can change
+        let mut guess = String::new(); // returns new instance of a string, apply the new function on the string via the :: syntax
 
-    // convert string to integer
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        io::stdin()
+            .read_line(&mut guess) // method to handle input from the user - take what the  users says and append it to the string
+            .expect("Failed to read line"); // this runs when the enum result is err, else it returns the input 
+        // read_line() returns an enum and encode info according to two state ok - and err.
+        // ok is linked to ouput and err linked to error msg and why it failed
+        // curly braces - placeholder for dynamic data
 
-    println!("You guessed this number: {guess}");
+        // convert string to integer
+        // the u32 is unsigned int, 32 bit, parse it as so
+        // let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            // we use the match call and try to match either of the result enum => Ok or Err
+            Ok(num)=>num, // assign number and move on
+            Err(_)=> continue, // catch any and all errors and move on to next iteration
+        };
+        // the .expect is there is handle any failures in parsing
 
-    // got a mismatch type error since we are taking in string as user input
-    // but the secret number is a goddamn integer
-    match guess.cmp(&sec_num){
-        Ordering::Less => println!("Too small!"),
-        Ordering::Equal => println!("You guessed right!"),
-        Ordering::Greater => println!("Too large!"),
+        println!("You guessed this number: {guess}");
+
+        // got a mismatch type error since we are taking in string as user input
+        // but the secret number is a goddamn integer
+        match guess.cmp(&sec_num) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Equal =>{
+                println!("You guessed right!");
+                break;
+            } 
+            Ordering::Greater => println!("Too large!"),
+        }
     }
 
     // immutability from what I understand is pointing a variable to a value that cannot be changed so when you do use a method to modify the contents of the variable, instead a new object is created and the variable points to the new object and the old object is destroyed
